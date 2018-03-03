0ec1de81c5137568418790523c5c992db03d9fbe
jack petersen <jack@petersen.biz>
2018-03-03 11:52:01 +0100

    project created

---
+++ src/test/java/biz/petersen/rest/danishzipcodes/DanishZipCodesApplicationTests.java
@@ -0,0 +1,16 @@
+package biz.petersen.rest.danishzipcodes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DanishZipCodesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
